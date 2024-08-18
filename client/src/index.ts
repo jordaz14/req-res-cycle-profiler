32f67bce5b3c8e15c41359f9055eb38ab0f90d82
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-08-17 21:56:32 -0400

    Create connection to server

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,2 +1,22 @@
-const message: string = "Hello, TypeScript!";
-console.log(message);
+const url = "http://localhost:3000";
+
+interface Home {
+  message: string;
+}
+
+fetchData<Home>(url).then((result) => console.log(result));
+
+async function fetchData<T>(url: string): Promise<T | null> {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+
+    return response.json() as T;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
