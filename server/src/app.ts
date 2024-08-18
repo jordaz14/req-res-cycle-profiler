a6af0df1d3e2249b74f14e53a395f333dea467fd
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-08-17 21:55:59 -0400

    Install CORS & add express boilerplate

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+  res.send({ message: "Hello World" });
 });
 
 app.listen(port, () => {
