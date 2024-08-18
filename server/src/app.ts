9c787a62f7f2ff885e6873f0a57e80766d87464a
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-08-18 00:09:50 -0400

    Add express boilderplate + track server processing time

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,18 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send({ message: "Hello World" });
+  const requestTime: number = Date.now();
+  const formattedRequestTime: string = new Date(requestTime).toISOString();
+
+  const LOOPCOUNT: number = 1e9;
+
+  for (let i = 0; i < LOOPCOUNT; i++) {
+    let j = 0;
+  }
+
+  const responseTime: number = Date.now();
+  const formattedResponseTime: string = new Date(responseTime).toISOString();
+  res.send({ reqTime: formattedRequestTime, resTime: formattedResponseTime });
 });
 
 app.listen(port, () => {
