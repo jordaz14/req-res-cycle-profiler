24efa2f90b45856a72a75055ea367ccb1663e1ec
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-08-21 17:25:55 -0400

    Change 'port' to 'PORT'

--- server/dist/app.js
+++ server/dist/app.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
