2e540da86be6014ff84cfe8b042ef71e58b064f4
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-08-22 10:00:22 -0400

    Create type declaration file for puppeteer-har

---
+++ client/types/puppeteer-har.d.ts
@@ -0,0 +1,15 @@
+// types/puppeteer-har.d.ts
+
+import { Page } from "puppeteer";
+
+interface PuppeteerHarOptions {
+  path?: string;
+}
+
+declare class PuppeteerHar {
+  constructor(page: Page, options?: PuppeteerHarOptions);
+  start(options?: PuppeteerHarOptions): Promise<void>;
+  stop(): Promise<void>;
+}
+
+export default PuppeteerHar;
