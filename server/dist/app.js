8d36854f1cea46b0352439e6fc06e751d3772f97
Julian <1julianordaz@gmail.com>
2024-08-22 20:27:41 -0400

    Add /generate-har endpoint

--- server/dist/app.js
+++ server/dist/app.js
@@ -4,22 +4,48 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const generateHar_1 = require("./generateHar");
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.get("/", (req, res) => {
-    const requestTime = Date.now();
-    const formattedRequestTime = new Date(requestTime).toISOString();
-    const LOOP_COUNT = 1e6;
-    for (let i = 0; i < LOOP_COUNT; i++) {
-        let j = 0;
+app.get("/generate-har", async (req, res) => {
+    try {
+        const harData = await (0, generateHar_1.generateHar)();
+        console.log(harData);
+        res.status(200).send({ message: "hi there" });
     }
-    const responseTime = Date.now();
-    const formattedResponseTime = new Date(responseTime).toISOString();
-    res.send({ reqTime: formattedRequestTime, resTime: formattedResponseTime });
+    catch (error) {
+        console.error("An error occurred.");
+    }
 });
+/*
+
+app.get("/", async (req: Request, res: Response) => {
+  try {
+    const requestTime: number = Date.now();
+    const formattedRequestTime: string = new Date(requestTime).toISOString();
+
+    const LOOP_COUNT: number = 1e6;
+
+    for (let i = 0; i < LOOP_COUNT; i++) {
+      let j = 0;
+    }
+
+    const responseTime: number = Date.now();
+    const formattedResponseTime: string = new Date(responseTime).toISOString();
+    res.send({
+      reqTime: formattedRequestTime,
+      resTime: formattedResponseTime,
+    });
+  } catch (error) {
+    console.error(error);
+    res.send("An error occurred");
+  }
+});
+
+*/
 app.listen(Number(port), "0.0.0.0", () => {
     console.log(`Server is listening on ${port}`);
 });
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
+import { generateHar } from "./generateHar";
 import cors from "cors";
+import { error } from "console";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,20 +9,42 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  const requestTime: number = Date.now();
-  const formattedRequestTime: string = new Date(requestTime).toISOString();
-
-  const LOOP_COUNT: number = 1e6;
-
-  for (let i = 0; i < LOOP_COUNT; i++) {
-    let j = 0;
+app.get("/generate-har", async (req: Request, res: Response) => {
+  try {
+    const harData = await generateHar();
+    console.log(harData);
+    res.status(200).send({ message: "hi there" });
+  } catch (error) {
+    console.error("An error occurred.");
   }
-
-  const responseTime: number = Date.now();
-  const formattedResponseTime: string = new Date(responseTime).toISOString();
-  res.send({ reqTime: formattedRequestTime, resTime: formattedResponseTime });
 });
+
+/*
+
+app.get("/", async (req: Request, res: Response) => {
+  try {
+    const requestTime: number = Date.now();
+    const formattedRequestTime: string = new Date(requestTime).toISOString();
+
+    const LOOP_COUNT: number = 1e6;
+
+    for (let i = 0; i < LOOP_COUNT; i++) {
+      let j = 0;
+    }
+
+    const responseTime: number = Date.now();
+    const formattedResponseTime: string = new Date(responseTime).toISOString();
+    res.send({
+      reqTime: formattedRequestTime,
+      resTime: formattedResponseTime,
+    });
+  } catch (error) {
+    console.error(error);
+    res.send("An error occurred");
+  }
+});
+
+*/
 
 app.listen(Number(port), "0.0.0.0", () => {
   console.log(`Server is listening on ${port}`);
