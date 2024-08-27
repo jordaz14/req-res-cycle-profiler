6e289068ceb88fc6b5d4068068af23078ccfa558
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-08-27 10:36:04 -0400

    Add nav link file content

---
+++ client/dist/server.js
@@ -0,0 +1,5 @@
+export const server = {
+    p_1: "This is a paragraph describing what the server section does.",
+    h2_1: "DNS Resolution Time",
+    p_2: "This is a paragraph describing what DNS resolution time does.",
+};
---
+++ client/src/client-server.ts
@@ -0,0 +1,5 @@
+export const clientServer = {
+  p_1: "This is a paragraph describing what the clientServer section does.",
+  h2_1: "DNS Resolution Time",
+  p_2: "This is a paragraph describing what DNS resolution time does.",
+};
