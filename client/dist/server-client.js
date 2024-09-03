85c667ccc3e4b248c9b324bf84f4accf82052268
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-03 10:19:52 -0400

    Remove excess files

--- client/dist/server-client.js
+++
@@ -1,5 +0,0 @@
-export const serverClient = {
-    p_1: "This is a paragraph describing what the server-client section does.",
-    h2_1: "Reponse Sending Time",
-    p_2: "This is a paragraph describing what DNS resolution time does.",
-};
--- client/src/client.ts
+++
@@ -1,5 +0,0 @@
-export const client = {
-  p_1: "This is a paragraph describing what the client section does.",
-  h2_1: "DNS Resolution Time",
-  p_2: "This is a paragraph describing what DNS resolution time does.",
-};
