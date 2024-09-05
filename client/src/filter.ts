ab4a9ff6e2946ef9d031f605fcf0590129be530d
Julian <1julianordaz@gmail.com>
2024-09-04 21:20:35 -0400

    Create different client side algos with differing time complexity

--- client/src/filter.ts
+++ client/src/filter.ts
@@ -4,7 +4,7 @@ export function initFilter(): any {
   console.log("filter.ts loaded");
 }
 
-// TEMPLATE FOR POST PAYLOADS
+// REQUEST PAYLOADS FILTER
 const smallJsonData = {
   id: 1,
   recipientName: "Chris Nolan",
@@ -31,13 +31,59 @@ function addJsonData(json: any, loop: any, data: any) {
 addJsonData(mediumJsonData, 10000, smallJsonData);
 addJsonData(largeJsonData, 100, mediumJsonData);
 
+const smallJsonDataSize = new Blob([JSON.stringify(smallJsonData)]).size;
+const mediumJsonDataSize = new Blob([JSON.stringify(mediumJsonData)]).size;
+const largeJsonDataSize = new Blob([JSON.stringify(largeJsonData)]).size;
+
+console.log("data", smallJsonDataSize, mediumJsonDataSize, largeJsonDataSize);
+
+// CLIENT SIDE ALGO COMPLEXITY
+
+const linearTimeAlgo = (arr: number[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    let num = i;
+  }
+};
+
+const quadraticTimeAlgo = (arr: number[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      let num = i + j;
+    }
+  }
+};
+
+const cubicTimeAlgo = (arr: number[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      for (let k = 0; k < arr.length; k++) {
+        let num = i + j + k;
+      }
+    }
+  }
+};
+
 export const filters = {
-  region: { status: "useast", us_west: "", us_east: "", asia: "" },
   req_payload: {
-    status: "large",
+    status: "small",
     small: smallJsonData,
     medium: mediumJsonData,
     large: largeJsonData,
+  },
+  server_alg: {
+    status: "linear",
+  },
+  sql: {
+    status: "low",
+  },
+  res_payload: {
+    status: "small",
+  },
+  client_alg: {
+    status: "linear",
+    linear: linearTimeAlgo,
+    quadratic: quadraticTimeAlgo,
+    cubic: cubicTimeAlgo,
   },
 };
 
